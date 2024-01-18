1e2a072dd94b21d5f374a722700fe9cacdd44dad
mdxv <mohan.elias@proton.me>
2024-01-18 20:12:03 -0300

    CArds fixed alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -233,9 +233,9 @@ export default function Home() {
                 {/* Text */}
                 <div >
                   <h2 className="text-2xl text-slate-200 font-bold mb-1 px-3 pt-5">Luciana Advocacia</h2>
-                 <p className='font-normal p-3'>Site estático desenvolvido para site de Consultoria Jurídica.</p>
+                 <p className='font-normal p-3'>Site estático dedicado à Consultoria Jurídica, projetado para fornecer informações claras e acessíveis sobre os serviços jurídicos oferecidos.</p>
 
-              <div className='grid grid-cols-7 mt-16'>
+              <div className='grid grid-cols-7 mt-4'>
               <div className='transition ease-in-out duration-500 react-logo hover:-translate-y-2'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><g fill="none"><g fill="#ffffff" clip-path="url(#akarIconsHtmlFill0)"><path d="M5.08 0h1.082v1.069h.99V0h1.082v3.236H7.152V2.153h-.99v1.083H5.081zm4.576 1.073h-.952V0h2.987v1.073h-.953v2.163H9.656zM12.165 0h1.128l.694 1.137L14.68 0h1.128v3.236h-1.077V1.632l-.744 1.151h-.019l-.745-1.15v1.603h-1.058zm4.181 0h1.083v2.167h1.52v1.07h-2.603z"></path><path fill-rule="evenodd" d="M5.046 22.072L3 4.717h18L18.953 22.07L11.99 24zm4.137-9.5l-.194-2.18h8.145l.19-2.128H6.664l.574 6.437h7.377l-.247 2.76l-2.374.642h-.002l-2.37-.64l-.152-1.697H7.332l.298 3.342l4.36 1.21l4.367-1.21l.532-5.964l.052-.571z" clip-rule="evenodd"></path></g><defs><clipPath id="akarIconsHtmlFill0"><path fill="#fff" d="M0 0h24v24H0z"></path></clipPath></defs></g></svg>
               </div>
